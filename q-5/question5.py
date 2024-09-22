cbf72a1be56978ac1194f8b9ddbb4c05ca0b0b72
AbhinavMangalore16 <abhinavm16104@gmail.com>
2024-09-23 02:05:11 +0530

    q-4 done

--- q-5/question5.py
+++ q-5/question5.py
@@ -1,37 +1,14 @@
-import time 
-import threading
 from collections import defaultdict
-class RateLimiter:
-    def __init__(self):
-        self.requests = defaultdict(list)
-        self.lock = threading.Lock()
-    def allow_req(self, user_id):
-        current_time = time.time()
-        with self.lock:
-            self.requests[user_id] = [timestamp for timestamp in self.requests[user_id] if current_time-timestamp<60]
-            if len(self.requests[user_id])<5:
-                self.requests[user_id].append(current_time)
-                return True
-            return False
-        
-#Simulating some requests to demonstrate the situation
-def rate_limiting(user_id, rate_limiter):
-    for i in range(20):
-        allowed = rate_limiter.allow_req(user_id)
-        print(f"Request {i+1} for {user_id}: {'Allowed' if allowed else 'Denied'}")
-        time.sleep(5)
+from typing import List, Dict, Callable
+def aggregation(data: List[Dict], key:str, aggregator: Callable):
+    grouped_data = defaultdict(list)
+    for item in data:
+        grouped_data[item[key]].append(item)
+    d = {}
+    for k, v in grouped_data.items():
+        d[k] = aggregator(v)
+    return d
 
-rate_limiter = RateLimiter()
-
-threads = []
-for user in ['user1', 'user2', 'user3', 'user4']:  
-    thread = threading.Thread(target=rate_limiting, args=(user, rate_limiter))
-    threads.append(thread)
-
-#starting all threads
-for thread in threads:
-    thread.start()
-
-#waiting for all threads to finish
-for thread in threads:
-    thread.join()
+data = [{'category': 'fruit', 'price': 10}, {'category': 'fruit', 'price': 20}, {'category': 'vegetable', 'price': 15}]
+result = aggregation(data, 'category', lambda x: sum(item['price'] for item in x))
+print(result)
